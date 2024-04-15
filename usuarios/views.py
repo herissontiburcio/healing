a5c01281dd122842c0c1b492ba19a87ce9dd4c48
Herisson Tiburcio <herisson.tiburcio@gmail.com>
2024-04-15 13:23:26 -0300

    Iniciando o projeto

---
+++ usuarios/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get("email")
+        senha = request.POST.get("senha")
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        users = User.objects.filter(username=username)
+
+        if users.exists():
+            print('Erro 1')
+            return redirect('/usuarios/cadastro')
+
+        if senha != confirmar_senha:
+            print('Erro 2')
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 6:
+            print('Erro 3')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            print('Erro 4')
+            return redirect('/usuarios/cadastro')
