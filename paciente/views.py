93334c44681b83fd8b3e4f7a8797fc4f7c151f3c
Herisson Tiburcio <herisson.tiburcio@gmail.com>
2024-04-17 11:18:43 -0300

    features

---
+++ paciente/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from medico.models import DadosMedico, Especialidades
+
+# Create your views here.
+def home(request):
+    if request.method == "GET":
+        
+        medico_filtrar = request.GET.get('medico')
+        especialidades_filtrar = request.GET.getlist('especialidades')
+        
+        medicos = DadosMedico.objects.all()
+    
+        if medico_filtrar:
+           medicos = medicos.filter(nome__icontains = medico_filtrar)
+       
+        especialidades = Especialidades.objects.all()
+    
+        if especialidades_filtrar:
+           medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
+    
+    return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
